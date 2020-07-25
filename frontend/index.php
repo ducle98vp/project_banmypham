8f5d94d0e450524c91ffff635c336f67dfd77667
Your Name <ducop98vp@gmail.com>
2020-07-25 18:20:54 +0800

    thay doi file index

--- frontend/index.php
+++ frontend/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-date_default_timezone_set("Asia/Ho_Chi_Minh");
+date_default_timezone_set("Asia/Ho_Chi_Minh"); 
 
 //mục đích của file index.php gốc của ứng dụng
 //cần phải xử lý url trên trình duyệt để nhúng được class
